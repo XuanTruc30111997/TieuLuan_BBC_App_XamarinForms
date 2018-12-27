e0e51d34415db0b56785520109047fafb75f2d06
Nguyen Trúc <NguyenXuanTruc30111997@gmail.com>
2018-12-27 11:37:49 +0700

    Update CheckAnswer

---
+++ bbc/bbc.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bbc.Data.Services;
+using bbc.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace bbc.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private RestTopicService restTopicService = null;
+        [TestMethod]
+        public void GetTopicTest()
+        {
+           
+        }
+    }
+}
